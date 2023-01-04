a029e054c5721a776f794bc8c6406800eed8cadf
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2023-01-04 17:11:37 +0900

    laravel icon added.

--- resources/views/profile/partials/update-card-information-form.blade.php
+++ resources/views/profile/partials/update-card-information-form.blade.php
@@ -1,11 +1,11 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('My Links') }}
+            {{ __('Edit My Card') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __("Update your links you want to display.") }}
+            {{ __("Update your public profile card.") }}
         </p>
     </header>
 
@@ -32,11 +32,10 @@
         </div>
     {!! Form::close() !!}
 
-    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-8 space-y-2">
         @csrf
         @method('patch')
-
-            <div>
+        <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
@@ -48,7 +47,7 @@
                     x-data="{ show: true }"
                     x-show="show"
                     x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
+                    x-init="setTimeout(() => show = false, 5000)"
                     class="text-sm text-gray-600"
                 >{{ __('Saved.') }}</p>
             @endif
@@ -56,17 +55,17 @@
 
     </form>
 
-    <form method="post" action="{{ route('links.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('links.update') }}" class="mt-8">
         @csrf
         @method('patch')
         <div id="dynamic-table">
-                        <script type="module">
+            <script type="module">
                 const existing = '{{$links->count()}}';
                 var i = existing == '0' ? 1 : existing;
                 $("#add-button").click(function () {
                     ++i;
                     const forms_to_add = `
-                        <div>
+                        <div class="mt-8 space-y-2">
                             <x-input-label for="links[${i}][title]" :value="'Title ${i}'" />
                             <x-text-input id="links[${i}][title]" name="links[${i}][title]" type="text" class="mt-1 block w-full" />
                             <x-input-label for="links[${i}][url]" :value="'URL ${i}'" />
@@ -82,36 +81,38 @@
             </script>
             @if($links->count() > 0)
                 @foreach ($links as $link)
-                <div>
-                    <x-text-input id="{{'links['.$loop->index.'][id]'}}" name="{{'links['.$loop->index.'][id]'}}" type="hidden" class="mt-1 block w-full" :value="$link->id" />
+                <div class="mt-8 space-y-2">
+                    <x-text-input id="{{'links['.$loop->index.'][id]'}}" name="{{'links['.$loop->index.'][id]'}}" type="hidden" class="" :value="$link->id" />
                     <x-input-label for="{{'links['.$loop->index.'][title]'}}" :value="'Title ' . ($loop->index)+1" />
                     <x-text-input id="{{'links['.$loop->index.'][title]'}}" name="{{'links['.$loop->index.'][title]'}}" type="text" class="mt-1 block w-full" :value="old('Title', $link->title)" />
                     <x-input-label for="{{'links['.$loop->index.'][url]'}}" :value="'URL ' . ($loop->index)+1" />
                     <x-text-input id="{{'links['.$loop->index.'][url]'}}" name="{{'links['.$loop->index.'][url]'}}" type="url" class="mt-1 block w-full" :value="old('URL', $link->url)" />
                     <x-input-error class="mt-2" :messages="$errors->get('links')" />
-                    <x-secondary-button type="button" name="remove" class=".remove-input-field">Remove</x-secondary-button>
+                    <x-secondary-button type="button" name="remove" class="remove-input-field">Remove Link</x-secondary-button>
                 </div>
                 @endforeach
             @else
                 <!-- displays forms to input user's first url. -->
-                <div>
+                <div class="mt-8 space-y-2">
                     <x-input-label for="links[1][title]" :value="'Title 1'" />
                     <x-text-input id="links[1][title]" name="links[1][title]" type="text" class="mt-1 block w-full" />
                     <x-input-label for="links[1][url]" :value="'URL 1'" />
                     <x-text-input id="links[1][url]" name="links[1][url]" type="url" class="mt-1 block w-full" />
-                    <x-secondary-button type="button" name="remove" class=".remove-input-field">Remove</x-secondary-button>
+                    <x-secondary-button type="button" name="remove" class="remove-input-field">Remove</x-secondary-button>
                 <div>
             @endif
         </div>
-        <x-secondary-button type="button" name="add" id="add-button" class="">Add</x-secondary-button>
-        <div class="flex items-center gap-4">
+        <div class="mt-8">
+            <x-secondary-button type="button" name="add" id="add-button" class="">+ Add New Link</x-secondary-button>
+        </div>
+        <div class="mt-8 flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
             @if (session('status') === 'card-updated')
                 <p
                     x-data="{ show: true }"
                     x-show="show"
                     x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
+                    x-init="setTimeout(() => show = false, 5000)"
                     class="text-sm text-gray-600"
                 >{{ __('Saved.') }}</p>
             @endif
