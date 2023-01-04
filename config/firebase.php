23546d6d0e4d44002d42db97fd58a9905f739ede
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2023-01-04 19:33:52 +0900

    env variable config added.

---
+++ config/firebase.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'databaseUrl' => env('VITE_FIREBASE_DATABASE_URL'),
+    'apiKey' => env('VITE_FIREBASE_API_KEY'),
+    'authDomain' => env('VITE_FIREBASE_AUTH_DOMAIN'),
+    'projectId' => env('VITE_FIREBASE_PROJECT_ID'),
+    'storageBucket' => env('VITE_FIREBASE_STORAGE_BUCKET'),
+    'messagingSenderId' => env('VITE_FIREBASE_MESSAGING_SENDER_ID'),
+    'aAppId' => env('VITE_FIREBASE_APPID'),
+    'measurementId' => env('VITE_FIREBASE_MEASUREMENT_ID'),
+];
