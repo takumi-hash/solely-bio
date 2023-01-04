5bfeb746499c9b0adeef97f5266ba22909c82141
takumi-hash <39505885+takumi-hash@users.noreply.github.com>
2023-01-04 16:41:42 +0900

    minor fix

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="rounded-lg bg-white p-8">
+<div class="rounded-lg shadow bg-white p-8">
     <img width="200" height="200" class="rounded-full mx-auto my-4" src="{{ isset($user->imageUrl) ? $user->imageUrl : "https://placehold.jp/ff5e00/ffffff/150x150.png" }}" />
     <h1 class="text-2xl text-slate-800 text-center">
         {{ $user->name }}
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden">
                 {{ $slot }}
             </div>
         </div>
